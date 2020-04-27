6ec5fa71639f6d868cad64576b88924f52cde42b
unknown <kevinshin123@gmail.com>
2020-04-27 17:14:46 +0900

    made some changes to line 5

--- assignment.c
+++ assignment.c
@@ -2,7 +2,7 @@
 
 int main(void){
 	printf("Hello world!\n");
-	printf("master\n");
+	printf("master, some changes in nbranch3\n"); printf("This line may corrupt\n");
 	printf("change in nbranch1\n");
 	printf("some change in nbranch2\n");
 
